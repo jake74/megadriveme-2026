b98daf27fb30c8f0f37eaae196d83df85d59b485
Jake Smith <jake@dekiru.uk>
2026-08-06 17:46:48 +0100

    Update post thumbnail size for game covers to 'showcase' across multiple post types

--- template-parts/card-game-cover.php
+++ template-parts/card-game-cover.php
@@ -1,6 +1,6 @@
 <?php if (get_post_type() === 'mega-drive') : ?>
   <a href="<?php the_permalink(); ?>" class="cover-md game-cover">
-    <?php the_post_thumbnail( 'md_cover', array( 'alt' => the_title_attribute( array( 'echo' => false, ) ), ) ); ?>
+    <?php the_post_thumbnail( 'showcase', array( 'alt' => the_title_attribute( array( 'echo' => false, ) ), ) ); ?>
     <div class="entry-header">
       <?php
       the_title( '<p class="entry-title">', '</p>' ); ?>
@@ -8,21 +8,21 @@
   </a>
 <?php elseif (get_post_type() === 'mega-cd') : ?>
   <a href="<?php the_permalink(); ?>" class="cover-mega-cd game-cover">
-    <?php the_post_thumbnail( 'mcd_cover', array( 'alt' => the_title_attribute( array( 'echo' => false, ) ), ) ); ?>
+    <?php the_post_thumbnail( 'showcase', array( 'alt' => the_title_attribute( array( 'echo' => false, ) ), ) ); ?>
     <div class="entry-header">
       <?php the_title( '<p class="entry-title">', '</p>' );?>
     </div>
   </a>
 <?php elseif (get_post_type() === '32x') : ?>
   <a href="<?php the_permalink(); ?>" class="cover-32x game-cover">
-    <?php the_post_thumbnail( 'md_cover', array( 'alt' => the_title_attribute( array( 'echo' => false, ) ), ) ); ?>
+    <?php the_post_thumbnail( 'showcase', array( 'alt' => the_title_attribute( array( 'echo' => false, ) ), ) ); ?>
     <div class="entry-header">
       <?php the_title( '<p class="entry-title">', '</p>' );?>
     </div>
   </a>
 <?php elseif (get_post_type() === 'hardware') : ?>
   <a href="<?php the_permalink(); ?>" class="cover-hardware game-cover">
-    <?php the_post_thumbnail( 'md_cover', array( 'alt' => the_title_attribute( array( 'echo' => false, ) ), ) ); ?>
+    <?php the_post_thumbnail( 'showcase', array( 'alt' => the_title_attribute( array( 'echo' => false, ) ), ) ); ?>
     <div class="entry-header">
       <?php the_title( '<p class="entry-title">', '</p>' );?>
     </div>
