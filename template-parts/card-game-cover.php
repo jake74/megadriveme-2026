eb7f3b135eb248ac5f28aef0a080e3c5d5e8d7c1
Jake Smith <jake@dekiru.uk>
2026-08-06 18:43:52 +0100

    Add data-post-type attribute to game cover links for better identification
    - Enhanced the game cover links for post types 'mega-drive', 'mega-cd', '32x', and 'hardware' by adding a data attribute that holds the post type label.
    - This change improves the accessibility and functionality of the game cover elements in the template.

--- template-parts/card-game-cover.php
+++ template-parts/card-game-cover.php
@@ -1,10 +1,12 @@
 <?php
 $post_type = get_post_type();
 // echo $post_type;
+
+$post_type_labels = get_post_type_object(get_post_type())->labels->menu_name;
 ?>
 
 <?php if ($post_type === 'mega-drive') : ?>
-  <a href="<?php the_permalink(); ?>" class="cover-md game-cover">
+  <a href="<?php the_permalink(); ?>" class="cover-md game-cover" data-post-type="<?php echo $post_type_labels; ?>">
     <?php the_post_thumbnail( 'showcase', array( 'alt' => the_title_attribute( array( 'echo' => false, ) ), ) ); ?>
     <div class="entry-header">
       <?php
@@ -12,21 +14,21 @@ $post_type = get_post_type();
     </div>
   </a>
 <?php elseif ($post_type === 'mega-cd') : ?>
-  <a href="<?php the_permalink(); ?>" class="cover-mega-cd game-cover">
+  <a href="<?php the_permalink(); ?>" class="cover-mega-cd game-cover" data-post-type="<?php echo $post_type_labels; ?>">
     <?php the_post_thumbnail( 'showcase_cd', array( 'alt' => the_title_attribute( array( 'echo' => false, ) ), ) ); ?>
     <div class="entry-header">
       <?php the_title( '<p class="entry-title">', '</p>' );?>
     </div>
   </a>
 <?php elseif ($post_type === '32x') : ?>
-  <a href="<?php the_permalink(); ?>" class="cover-32x game-cover">
+  <a href="<?php the_permalink(); ?>" class="cover-32x game-cover" data-post-type="<?php echo $post_type_labels; ?>">
     <?php the_post_thumbnail( 'showcase', array( 'alt' => the_title_attribute( array( 'echo' => false, ) ), ) ); ?>
     <div class="entry-header">
       <?php the_title( '<p class="entry-title">', '</p>' );?>
     </div>
   </a>
 <?php elseif ($post_type === 'hardware') : ?>
-  <a href="<?php the_permalink(); ?>" class="cover-hardware game-cover">
+  <a href="<?php the_permalink(); ?>" class="cover-hardware game-cover" data-post-type="<?php echo $post_type; ?>">
     <?php the_post_thumbnail( 'showcase', array( 'alt' => the_title_attribute( array( 'echo' => false, ) ), ) ); ?>
     <div class="entry-header">
       <?php the_title( '<p class="entry-title">', '</p>' );?>
